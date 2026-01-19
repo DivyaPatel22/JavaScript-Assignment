44f1b7800a36a8f19b968e2f39a075bbe1658a78
DivyaPatel22 <dpatel.101122@gmail.com>
2026-01-19 12:18:07 +0530

    overview of javascript

---
+++ Self-Learning/Functions.js
@@ -0,0 +1,17 @@
+//function statement :
+//Function DEclaration also Function Statement
+
+function a(){
+    console.log("This is Function Statement !");
+}
+ 
+//Function Expression :
+
+var a = function b(){
+    console.log("This is Function Expression !");
+}
+
+//Difference between both of them is hoisted
+
+//Anonymous Function : fun. without name is ...   used when funtion are used as values (assigning a variable)
+
---
+++ Session-1/Overview.js
@@ -0,0 +1,16 @@
+// Javascript :
+
+// high-level : designed to be human-readable using English-like syntax
+// lightweight : designed to minimal resource use (memory/CPU),easy to implement, minimalist syntax
+// dynamic : interpreter assigns variables a type at runtime based on the variable's value at the time.
+// interpreted : executed line-by-line
+// just-in-time compiled : compilation process in which code is translated from an intermediate representation or a   higher-level language into machine code at runtime, rather than prior to execution.
+// Prototype-based programming : style of OOPS in which classes are not explicitly defined, but rather derived by adding properties and methods to an instance of another class       ---- allows the creation of an object without first defining its class.
+// garbage-collected : process of finding and deleting objects which are no longer being referenced by other objects.
+// imperative, functional, and object-oriented
+
+
+// Use-cases && Applications : 
+// Web-dev , Game-dev , Mobile/Desktop App, AI & IOT , Single Page Applications (SPAs)
+
+// core technology of the web, enabling interactive pages and complex applications
