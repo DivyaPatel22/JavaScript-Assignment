645b9e31efa44c423ca867f1e8173c1b2a30383a
DivyaPatel22 <dpatel.101122@gmail.com>
2026-03-25 15:25:04 +0530

    Session_6 Assignment Completed

---
+++ __React/Session_6/Session_06/src/App.jsx
@@ -0,0 +1,19 @@
+import InersectionObserverCompo from "./Components/Infinite Scroll/InersectionObserver"
+import InfiniteScrollDummy from "./Components/Infinite Scroll/InfiniteScrollDummy"
+import Task2 from "./Components/Pagination&Sorting/Task2"
+
+// import Task1 from "./Components/Serch(Debouse)/Task1"
+function App() {
+  
+
+  return (
+    <>
+      {/* <Task1></Task1> */}
+      {/* <Task2 /> */}
+      {/* <InfiniteScrollDummy /> */}
+      {/* <InersectionObserverCompo /> */}
+    </>
+  )
+}
+
+export default App
